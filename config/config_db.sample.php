34dc1e5eebc93d58ef33a637d663fa081eb60b89
sleeping-lion <toughjjh@gmail.com>
2015-03-26 21:43:15 +0900

    update

--- config/config_db.sample.php
+++ config/config_db.sample.php
@@ -1,21 +1,21 @@
 <?php
 
-if(MODE=='PRODUCTION') {
-/* 실제 데이터베이스 설정 */
-$config_db['hostname'] = 'localhost';
-$config_db['username'] = 'toughjjh';
-$config_db['database'] = 'framework';
-$config_db['password'] = 'jjh7479';
-$config_db['dbdriver'] = 'mysql';
-$config_db['charset'] = 'utf8';
-$config_db['dbcollat'] = 'utf8_general_ci';
+if (MODE == 'PRODUCTION') {
+	/* 실제 데이터베이스 설정 */
+	$config_db['hostname'] = 'localhost';
+	$config_db['username'] = 'username';
+	$config_db['database'] = 'database';
+	$config_db['password'] = 'password';
+	$config_db['dbdriver'] = 'mysql';
+	$config_db['charset'] = 'utf8';
+	$config_db['dbcollat'] = 'utf8_general_ci';
 } else {
-/* 개발  데이터베이스 설정 */
-$config_db['hostname'] = 'localhost';
-$config_db['username'] = 'toughjjh';
-$config_db['database'] = 'framework';
-$config_db['password'] = 'jjh7479';
-$config_db['dbdriver'] = 'mysql';
-$config_db['charset'] = 'utf8';
-$config_db['dbcollat'] = 'utf8_general_ci';
+	/* 개발  데이터베이스 설정 */
+	$config_db['hostname'] = 'localhost';
+	$config_db['username'] = 'username';
+	$config_db['database'] = 'database';
+	$config_db['password'] = 'password';
+	$config_db['dbdriver'] = 'mysql';
+	$config_db['charset'] = 'utf8';
+	$config_db['dbcollat'] = 'utf8_general_ci';
 }
